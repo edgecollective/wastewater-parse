e30303d6046260b21aaec5110565f924191818a3
Don Blair <donblair@pvos.org>
2022-02-19 15:10:14 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # wastewater-parse
-
-MWRA parsing
---
+++ parse.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import camelot
+import pandas as pd
+
+url = 'https://www.mwra.com/biobot/biobotdata.htm'
+data_url = ''
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+
+urls = []
+for link in soup.find_all('a'):
+    #print('trick')
+    this_link=link.get('href')
+    if (this_link != None):
+        #print(link.get('href'))
+        if (this_link.find('-data') != -1):
+            #print("got it!")
+            data_url='https://www.mwra.com/biobot/'+this_link
+
+if len(data_url)==0:
+    print('no data url found')
+else:
+
+    tables = camelot.read_pdf(data_url, pages = "1-end")
+    frames=[]
+    for i in range(0,len(tables)):
+        frames.append(tables[i].df)
+    df = pd.concat(frames)
+    df = df[1:]
+    df.columns = ['Date','Southern','Northern','Southern_7','Northern_7','Southern_low','Southern_high','Northern_low','Northern_high','Southern_variant','Northern_variant']
+    #print(df.tail)
+
+    #dates = df['Date'].to_list()
+    #northern = df['Northern'].to_list()
+    #southern = df['Southern'].to_list()
+    df.to_csv('waste.csv',index=False)
