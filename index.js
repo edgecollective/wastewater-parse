38404972d4ce14c9f4edd9b22c86a2a2bd07f71d
Don Blair <donblair@gmail.com>
2022-02-19 13:10:22 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+var fs = require('fs');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 async function lsWithGrep() {
   try {
+      let ts = Date.now();
       console.log('grabbing ...');
       const { stdout, stderr } = await exec('./grab.sh');
       console.log('stdout:', stdout);
       console.log('stderr:', stderr);
+      log_file.write(ts+'\n');
+      log_file.write(stdout + '\n');
+      log_file.write(stderr);
   } catch (error) {
      console.error(error);
+     let ts = Date.now();
+     log_file.write(ts+'\n');
+     log_file.write(error + '\n');
+     log_file.write(stdout + '\n');
+     log_file.write(stderr);
   };
 };
 
-lsWithGrep();
-//setInterval(lsWithGrep(),60000);
+//lsWithGrep();
+setInterval(lsWithGrep,60000);
